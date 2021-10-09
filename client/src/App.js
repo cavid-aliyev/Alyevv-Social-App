d61a13ed6d2dbd4a47c26bc4ca723af2cae7dbb5
Alyeev <javidalyevv@gmail.com>
2021-10-09 11:49:51 +0400

    Add mongoose set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const App = () => {
+
   return (
     <div>
       <h1>App</h1>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://javidalyev:fabregas056@cluster0.nohrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
