f3c5a6033faa86b05acd55814e6d083c20b9ab0a
Alyeev <javidalyevv@gmail.com>
2021-10-09 13:51:56 +0400

    Add redux && api servces

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import "./assets/styles/globalStyles.css";
+
+//components
+import Posts from "./components/Posts";
+import Form from "./components/Form";
+
+import memories from "./assets/images/memories.png";
 
 const App = () => {
-
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+    <Container maxWidth="lg">
+      <AppBar className="app-bar" position="static" color="inherit">
+        <Typography className="app-heading" variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className="app-image" src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
-export default App
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+//redux stuffs
+import { reducers } from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
